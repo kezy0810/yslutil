de95387e754c7a77312dc2b799b1ff4bfb5b5252
zhangchunming <zhangchunming@PC201607151026>
2016-11-01 17:50:24 +0800

    添加订单号生成器

--- src/main/java/com/qkl/util/help/OrderGenerater.java
+++ src/main/java/com/qkl/util/help/OrderGenerater.java
@@ -12,8 +12,23 @@ public class OrderGenerater {
     public static String generateOrderNo(){
     	StringBuilder sb = new StringBuilder();
         SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss");
-        sb.append(sf.format(new Date()))
-            .append(RandomStringUtils.randomNumeric(6));
+        sb.append(sf.format(new Date())).append(RandomStringUtils.randomNumeric(6));
+        return sb.toString();
+    }
+    /**
+     * @describe:订单号规则：日期+6位随机数+邀请码（唯一）
+     * @author: zhangchunming
+     * @date: 2016年11月1日下午5:47:23
+     * @param user_code
+     * @return: String
+     */
+    public static String generateOrderNo(String user_code){
+        if("".equals(user_code)||user_code == null){
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        SimpleDateFormat sf = new SimpleDateFormat("yyMMddHHmmss");
+        sb.append(sf.format(new Date())).append(RandomStringUtils.randomNumeric(6)).append(user_code);
         return sb.toString();
     }
 }
